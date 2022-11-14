4cb3e10645fe2c79559bc0ab52f332fb06692d87
Lemain99 <102553300+Lemain99@users.noreply.github.com>
2022-11-14 21:50:21 +0300

    Added liquibase

---
+++ src/main/resources/liquibase/sql/init_database.sql
@@ -0,0 +1,76 @@
+create table if not exists academic_discipline
+(
+    id                bigint not null
+        primary key,
+    discipline_number varchar(255),
+    name              varchar(255)
+);
+create sequence if not exists academic_discipline_seq
+    increment by 50;
+create table if not exists academic_methods
+(
+    id                     bigint not null
+        primary key,
+    activity_type          varchar(255),
+    complete_info          varchar(255),
+    dead_line              date,
+    academic_discipline_id bigint not null
+        constraint fkaqkuujflwbrksut4tdqrt5t5u
+            references academic_discipline,
+    specialization_id      bigint not null
+        constraint fkfvfon5jaw36leq4n4th07i2ql
+            references specialization
+);
+create sequence if not exists methods_work_seq
+    increment by 50;
+create table if not exists academic_work
+(
+    id                     bigint not null
+        primary key,
+    absolute_results       real,
+    group_name             varchar(255),
+    incomplete_reason      varchar(255),
+    quality_results        real,
+    academic_discipline_id bigint not null
+        constraint fkkkkpm2fh5am9x0ecnh5mmj91i
+            references academic_discipline,
+    specialization_id      bigint not null
+        constraint fkr11j2dxpiqalwbtge4po5ei5x
+            references specialization
+);
+create sequence if not exists academic_work_seq
+    increment by 50;
+create table if not exists department
+(
+    id   bigint not null
+        primary key,
+    name varchar(255)
+);
+create sequence if not exists department_seq
+    increment by 50;
+create table if not exists specialization
+(
+    id                  bigint not null
+        primary key,
+    specialization      varchar(255),
+    specialization_name varchar(255)
+);
+create sequence if not exists specialization_seq
+    increment by 50;
+create table if not exists teacher
+(
+    id                 bigint not null
+        primary key,
+    category           varchar(255),
+    certification_date date,
+    employment_type    varchar(255),
+    firstname          varchar(255),
+    lastname           varchar(255),
+    middlename         varchar(255),
+    position           varchar(255),
+    department_id      bigint not null
+        constraint fkd419q6obhj46eoye136y7rjyq
+            references department
+);
+create sequence if not exists teacher_seq
+    increment by 50;

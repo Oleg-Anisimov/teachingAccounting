872539ce89f0e78d2f7c5610739bdc7705cd88d8
Raboofoce <dragoblidzi2580@gmail.com>
2022-11-16 17:11:03 +0300

    Fixed DataBaseInitializer

--- src/main/java/me/anisimov/teachingAccounting/TeachingAccountingApplication.java
+++ src/main/java/me/anisimov/teachingAccounting/TeachingAccountingApplication.java
@@ -1,13 +1,26 @@
 package me.anisimov.teachingAccounting;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import me.anisimov.teachingAccounting.util.DataBaseInitializer;
+
 
 @SpringBootApplication
-public class TeachingAccountingApplication {
+public class TeachingAccountingApplication implements CommandLineRunner {
 
+	@Autowired
+	private DataBaseInitializer dataBaseInitializer;
 	public static void main(String[] args) {
 		SpringApplication.run(TeachingAccountingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		dataBaseInitializer.init();
+
 	}
 
 }
--- src/main/java/me/anisimov/teachingAccounting/domain/Department.java
+++ src/main/java/me/anisimov/teachingAccounting/domain/Department.java
@@ -1,5 +1,6 @@
 package me.anisimov.teachingAccounting.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ public class Department {
     @Column(name="name")
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "department")
     private List<Teacher> teachers;
 }

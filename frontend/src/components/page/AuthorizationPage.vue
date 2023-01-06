f2e6381707aa7ecb23beb238ea22660e1c078b30
Anton Chernyshev <nascar32@yandex.ru>
2023-01-07 01:23:12 +0700

    auth changed

--- frontend/src/components/page/AuthorizationPage.vue
+++ frontend/src/components/page/AuthorizationPage.vue
@@ -8,12 +8,13 @@
             <div class="ui-input-group">
                 <div class="ui-input">
                     <label for="input-login">Логин</label>
-                    <input id="input-login" type="text" v-model="this.model.username" placeholder="Введите логин" />
+                    <input id="input-login" type="text" v-model="this.model.username" placeholder="Введите логин"/>
                 </div>
                 <div class="ui-input">
                     <label for="input-password">Пароль</label>
                     <input id="input-password" type="password" v-model="this.model.password" placeholder="Введите пароль"/>
                 </div>
+              <p class="alertMessage" v-if="model.invalid">Неверный логин или пароль</p>
                 <div class="ui-input">
                     <button class="ui-button isDefault" @click="perfromLogin()">Вход</button>
                     <button class="ui-button isDefault"><router-link to="/registration">Зарегистрироваться</router-link></button>
@@ -32,7 +33,8 @@ export default{
       return {
         model: {
           username:'',
-          password:''
+          password:'',
+          invalid: false
         }
       }
     },
@@ -44,6 +46,8 @@ export default{
           password: this.model.password
         }).then((data) => {
           this.$router.push('title');
+        }).catch((e) => {
+          this.model.invalid = true
         })
       }
     },
@@ -54,6 +58,9 @@ export default{
 
 </script>
 <style scoped>
+.alertMessage{
+  color: red;
+}
 .authPage{
     background-color: #e1e1e1;
     padding-bottom: 26em;

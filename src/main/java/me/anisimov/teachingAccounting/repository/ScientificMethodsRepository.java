122030b61b80d4db254b9e5957c05eb276f095db
Lemain99 <102553300+Lemain99@users.noreply.github.com>
2023-01-06 15:18:00 +0300

    Added pagination for SM

--- src/main/java/me/anisimov/teachingAccounting/repository/ScientificMethodsRepository.java
+++ src/main/java/me/anisimov/teachingAccounting/repository/ScientificMethodsRepository.java
@@ -1,8 +1,11 @@
 package me.anisimov.teachingAccounting.repository;
 
 import me.anisimov.teachingAccounting.domain.AcademicMethods;
+import me.anisimov.teachingAccounting.domain.AcademicWork;
 import me.anisimov.teachingAccounting.domain.ScientificMethods;
 import me.anisimov.teachingAccounting.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +13,6 @@ import java.util.List;
 public interface ScientificMethodsRepository extends JpaRepository<ScientificMethods,Long> {
 
     List<ScientificMethods> findAllByUser(User user);
+
+    Page<ScientificMethods> getAllByUser(User user, Pageable pageable);
 }

f00826e93673fc05fd38830458ca7529e31697d0
Anton Chernyshev <nascar32@yandex.ru>
2023-01-11 18:11:16 +0700

    issue-116/added animation on pagination buttons

--- frontend/src/components/pogination/pog.vue
+++ frontend/src/components/pogination/pog.vue
@@ -5,18 +5,18 @@
             <div>
               <p>
                 (Всего: {{ totalElements }}) На страницу:
-                <input type="number" min="0" :max="totalElements" v-model="pageRequest.size"/>.
+                <input type="number" min="0" :max="totalElements" v-model="pageRequest.size"/>
               </p>
             </div>
             <div>
-              <button :disabled="pageRequest.page === 0" v-on:click="pageRequest.page -= 1">
-                ❮ Назад
+              <button :class="pageRequest.page === 0 ? '' :'back'" :disabled="pageRequest.page === 0" v-on:click="pageRequest.page -= 1">
+                <span>Назад</span>
               </button>
               {{ pageRequest.page + 1 }}
-              <button :disabled="pageRequest.page === totalPages - 1" v-on:click="pageRequest.page += 1">
-                Вперед ❯
+              <button :class="pageRequest.page === totalPages - 1 ? '' : 'next'" :disabled="pageRequest.page === totalPages - 1" v-on:click="pageRequest.page += 1">
+                <span>Вперед</span>
               </button>
-            </div>
+              </div>
             <div class="rightColumn">Страниц: {{ totalPages }}</div>
           </div>
         </td>
@@ -44,4 +44,56 @@ export default {
 .rightColumn {
   text-align: right;
 }
+
+.next span {
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.next span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -0.8em;
+  transition: 0.5s;
+
+}
+
+.next:hover span {
+  padding-left: 0em;
+}
+
+.next:hover span:after {
+  opacity: 1;
+  right: -1em;
+}
+.back span {
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.back span:after{
+  content: '\00AB';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: -0.8em;
+  transition: 0.5s;
+}
+
+.back:hover span {
+  padding-right: 0em;
+}
+
+.back:hover span:after {
+  opacity: 1;
+  left: -1em;
+}
+/*.qwe {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}*/
 </style>

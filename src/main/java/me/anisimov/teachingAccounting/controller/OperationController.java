80b6b6a13ed593228aa36bfb6c50dfcf00fca176
Ilya Shatalov <ilya@alov.me>
2023-01-13 21:39:40 +0300

    issue-43. Export to excel

--- src/main/java/me/anisimov/teachingAccounting/controller/OperationController.java
+++ src/main/java/me/anisimov/teachingAccounting/controller/OperationController.java
@@ -3,8 +3,12 @@ package me.anisimov.teachingAccounting.controller;
 import me.anisimov.teachingAccounting.domain.AcademicWork;
 import me.anisimov.teachingAccounting.domain.Teacher;
 import me.anisimov.teachingAccounting.domain.User;
+import me.anisimov.teachingAccounting.dto.AcademicWorkDto;
+import me.anisimov.teachingAccounting.repository.AcademicWorkRepository;
 import me.anisimov.teachingAccounting.repository.EntityRepository;
 import me.anisimov.teachingAccounting.repository.UserRepository;
+import me.anisimov.teachingAccounting.service.AcademicWorkService;
+import me.anisimov.teachingAccounting.service.ExportToExcelService;
 import me.anisimov.teachingAccounting.service.OperationService;
 import me.anisimov.teachingAccounting.util.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 //тестовый контроллер
@@ -21,19 +28,15 @@ import java.util.List;
 public class OperationController {
 
     @Autowired
-    private OperationService operationService;
-    @Autowired
-    private EntityRepository entityRepository;
-    @Autowired
-    private UserRepository userRepository;
+    private ExportToExcelService exportToExcelService;
 
+    @Autowired
+    private AcademicWorkService academicWorkService;
 
     @GetMapping
-    public ResponseEntity test() {
+    public void test() throws IOException {
 
-        String currentUsername = SecurityUtils.getCurrentUsername();
-        User user = userRepository.findUserByLogin(currentUsername);
-        List<AcademicWork> academicWorks = entityRepository.listForUser(AcademicWork.class, user);
-        return ResponseEntity.ok(academicWorks);
+        exportToExcelService.export("test111.xls", AcademicWorkDto.class, academicWorkService.getAll());
+
     }
 }

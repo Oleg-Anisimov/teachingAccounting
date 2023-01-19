b7a062e3e1ed3079688d23938977dbbffb03cc40
Disoldio <klish.andr@yandex.ru>
2023-01-19 21:36:41 +0300

    -fixed specialization/group/discipline connection

--- frontend/src/vuex/store.js
+++ frontend/src/vuex/store.js
@@ -1,6 +1,5 @@
 import {createStore} from "vuex";
 import axios from "axios";
-import qs from 'qs';
 import {user} from "./userStore";
 import {categoryBased} from "./categoryBased";
 import {academicWork} from "./pageStores/academicWork";
@@ -27,30 +26,83 @@ let store = createStore({
 
     state: {
         teacher: {},
+        specializations: [],
+        academicDisciplines: [],
     },
     mutations: {
         SET_TEACHER: (state, teacher) => {
             state.teacher = teacher
         },
+        SET_SPECIALIZATIONS: (state, specializations) => {
+            state.specializations = specializations
+        },
+        SET_ACADEMIC_DISCIPLINES: (state, academicDisciplines) => {
+            state.academicDisciplines = academicDisciplines
+        },
+        SET_GROUPS: (state, groups) => {
+            state.groups = groups;
+        },
     },
     actions: {
         LOAD_TEACHER({commit}) {
-                    let url = '/api/teacher'
-                    return axios.get(url)
-                        .then((response) => {
-                            commit('SET_TEACHER', response.data)
-                            return response.data
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                },
+            let url = '/api/teacher'
+            return axios.get(url)
+            .then((response) => {
+                commit('SET_TEACHER', response.data)
+                return response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        },
+        LOAD_SPECIALIZATION({commit}){
+            let url = '/api/specialization/all'
+            return axios(url, { method: 'GET'})
+            .then((specializations) => {
+                commit('SET_SPECIALIZATIONS', specializations.data)
+                return specializations.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        },
+        LOAD_ACADEMIC_DISCIPLINE({commit}){
+            let url = '/api/discipline/all'
+            return axios(url, { method: 'GET'})
+            .then((academicDisciplines) => {
+                commit('SET_ACADEMIC_DISCIPLINES', academicDisciplines.data)
+                return academicDisciplines.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        },
+        LOAD_GROUPS({commit}){
+            let url = '/api/group/all'
+            return axios(url, { method: 'GET'})
+                .then((groups) => {
+                    commit('SET_GROUPS', groups.data)
+                    return groups.data
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
 
 
     },
     getters: {
         GET_TEACHER(state) {
             return state.teacher
+        },
+        GET_ALL_SPECIALIZATIONS(state){
+            return state.specializations
+        },
+        GET_ALL_ACADEMIC_DISCIPLINES(state){
+            return state.academicDisciplines
+        },
+        GET_ALL_GROUPS(state) {
+            return state.groups
         },
     },
 })
